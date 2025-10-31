ed89251aad3ee88bd874afdd17ce40b1b0dd4998
Leny <lenypan2000@gmail.com>
2025-10-31 02:53:08 -0700

    Created a document frequency index as well

--- src/inverted_index.py
+++ src/inverted_index.py
@@ -1,7 +1,6 @@
-import pandas as pd
 import json
 from collections import defaultdict
-
+import math
 
 # Function to build inverted inddex from processed text
 def build_inverted_index(df, text_column, movie_column, extra_columns=None):
@@ -25,6 +24,12 @@ def build_inverted_index(df, text_column, movie_column, extra_columns=None):
 
     return inverted_index
 
+def build_doc_frequency(df, inverted_index, movie_column):
+    document_frequency = {}
+    total_docs = df[movie_column].nunique()
+    for token in inverted_index.items():
+        document_frequency[token] = math.log((total_docs + 1) / (len(inverted_index[token]) + 1)) + 1
+    return document_frequency
 
 # Letterboxd
 letterboxd_df = pd.read_csv("data/processed/letterboxd_reviews_clean.csv")
@@ -34,10 +39,18 @@ letterboxd_index = build_inverted_index(
     movie_column="movie_name",
     extra_columns=["rating", "like_count"]
 )
+letterboxd_docfreq = build_doc_frequency(
+    letterboxd_df,
+    inverted_index = letterboxd_index,
+    movie_column="movie_name"
+)
+
 
 with open("indexes/letterboxd_index.json", "w", encoding="utf-8") as f:
     json.dump(letterboxd_index, f, ensure_ascii=False, indent=2)
 
+with open("indexes/letterboxd_document_frequency.json", "w", encoding="utf-8") as f:
+    json.dump(letterboxd_docfreq, f, ensure_ascii=False, indent=2)
 
 # Metacritic
 metacritic_df = pd.read_csv("data/processed/metacritic_reviews_clean.csv")
@@ -46,6 +59,14 @@ metacritic_index = build_inverted_index(
     text_column="summary",
     movie_column="movie_name",
 )
+metacritic_docfreq = build_doc_frequency(
+    metacritic_df,
+    inverted_index = metacritic_index,
+    movie_column="movie_name"
+)
 
 with open("indexes/metacritic_index.json", "w", encoding="utf-8") as f:
     json.dump(metacritic_index, f, ensure_ascii=False, indent=2)
+
+with open("indexes/metacritic_document_frequency.json", "w", encoding="utf-8") as f:
+    json.dump(metacritic_docfreq, f, ensure_ascii=False, indent=2)
