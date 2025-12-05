fb7ea9ff0774d91116cb265fade992cfcb2d1353
Matthew Rimland <mrimland@gmail.com>
2025-12-04 22:02:59 -0600

    Adding in pytest basic unit tests and title search
    Adding in unit tests and basic title search

--- src/query_interface.py
+++ src/query_interface.py
@@ -1,5 +1,5 @@
 import os
-from query_processing import process_query
+from query_processing import process_query, process_title_query
 
 def main():
     print("\nMovie Search Engine\n")
@@ -19,6 +19,21 @@ def main():
         if query.lower() == "exit":
             print("\nGoodbye!\n")
             break
+        if query.lower() == "-help":
+            print("""The following is a sentiment based movie search engine. Enter a query about the emotional tone of a movie and\nwe will return the most relevant movies to your query.\nYou can also enter -title to do a standard title search to view available movies""")
+            continue
+        if query.lower() == "-title":
+            movie_prefix = input("Enter a movie title/prefix: ").strip()
+            title_results = process_title_query(movie_prefix)
+            if len(title_results) == 0:
+                print("""No movies found with that prefix""")
+            else:
+                print("\n" + "-" * 60 + "\n")
+                print("Movies:\n")
+                for i, movie in enumerate(title_results, start=1):
+                    print(f"{i:2d}. {movie:<40}")
+            continue
+
 
         if not query:
             continue
